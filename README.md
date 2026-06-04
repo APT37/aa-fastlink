a33303bd0042f693452ebdcfe645ef2e18914f59
APT37 <>
2026-06-04 10:04:42 +0200

    Add warning message to debug output option, reuse static client

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Share your Anna's Archive membership, but without leaking your secret key!
 
 ### Environment Variables
 
-| Variable           | Description                             | Default     |
-| ------------------ | --------------------------------------- | ----------- |
-| `AA_SECRET`        | Secret key, check `Account` page.       | none        |
-| `AA_DOMAIN`        | Mirror domain, e.g. `annas-archive.org` | none        |
-| `AA_BIND_IP`       | IP address to listen on.                | `127.0.0.1` |
-| `AA_BIND_PORT`     | Port to listen on.                      | `3030`      |
-| `AA_DEBUG_LOGGING` | Enable debug logging.                   | `false`     |
+| Variable           | Description                                                                      | Default     |
+| ------------------ | -------------------------------------------------------------------------------- | ----------- |
+| `AA_SECRET`        | Secret key, check `Account` page.                                                | none        |
+| `AA_DOMAIN`        | Mirror domain, e.g. `annas-archive.org`                                          | none        |
+| `AA_BIND_IP`       | IP address to listen on.                                                         | `127.0.0.1` |
+| `AA_BIND_PORT`     | Port to listen on.                                                               | `3030`      |
+| `AA_DEBUG_LOGGING` | Enable debug logging. __CAREFUL: Secret key is logged when printing responses!__ | `false`     |
 
 ### Web Interface
 
--- src/download.rs
+++ src/download.rs
@@ -1,7 +1,7 @@
 use crate::config;
 use log::{debug, error, warn};
 use serde::Deserialize;
-use std::{collections::HashMap, error::Error, fmt, str::FromStr};
+use std::{collections::HashMap, error::Error, fmt, str::FromStr, sync::LazyLock};
 use warp::{Reply, http::Uri, redirect, reject::Rejection, reply::Response};
 
 fn format_error(error: impl Error) -> String {
@@ -25,7 +25,7 @@ pub(super) async fn handle_download(form: HashMap<String, String>) -> Result<Res
 
             let hash = link_or_hash[link_or_hash.len() - 32..].to_string(); // needed?
 
-            match get_fast_download_link(&hash).await {
+            match fetch_fast_download_link(&hash).await {
                 Ok(fast_download_link) => Ok(redirect::see_other(
                     Uri::from_str(&fast_download_link).unwrap(),
                 )
@@ -76,23 +76,28 @@ impl fmt::Display for FetchError {
 
 impl Error for FetchError {}
 
-async fn get_fast_download_link(hash: &str) -> Result<String, Box<dyn Error>> {
-    debug!("getting fast_download_link for [{hash}] ({})", hash.len());
+static CLIENT: LazyLock<reqwest::Client> = LazyLock::new(reqwest::Client::new);
 
-    let response_result = reqwest::get(format!(
-        "https://{}/dyn/api/fast_download.json?md5={}&key={}",
-        *config::DOMAIN,
-        hash,
-        *config::SECRET
-    ))
-    .await;
+async fn fetch_fast_download_link(hash: &str) -> Result<String, Box<dyn Error>> {
+    debug!("fetching fast_download_link for [{hash}] ({})", hash.len());
+
+    let response_result = CLIENT
+        .get(format!(
+            "https://{}/dyn/api/fast_download.json?md5={}&key={}",
+            *config::DOMAIN,
+            hash,
+            *config::SECRET
+        ))
+        .send()
+        .await;
     debug!("{response_result:#?}");
 
-    let json_result = response_result?.json().await;
+    let json_result = response_result?
+        .error_for_status()?
+        .json::<ApiResponse>()
+        .await;
     debug!("{json_result:#?}");
-
-    let api_response: ApiResponse = json_result?;
-    debug!("{api_response:#?}");
+    let api_response = json_result?;
 
     if let Some(fast_download_link) = api_response.download_url {
         Ok(fast_download_link)
